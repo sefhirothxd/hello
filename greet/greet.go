3360dd2fdbc435023e19d9a15a5e5b76623f9c3a
bryan vera <bveracachay@gmail.com>
2021-09-06 17:34:21 -0500

    first commit

---
+++ greet/greet.go
@@ -0,0 +1,12 @@
+package greet
+
+var emoji = "👨‍🎓"
+
+//saludo en ingles
+func English() string {
+	return "Hi " + emoji
+}
+
+func Italian() string {
+	return "Chao " + emoji
+}
---
+++ greet/spanish.go
@@ -0,0 +1,6 @@
+package greet
+
+//saludo en espa;ol
+func Spanish() string {
+	return "Hola " + emoji
+}
